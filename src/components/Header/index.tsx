f40822161223f0e131ed37286d8dfaf57f2a81fb
Victor Hugo <victorhsmsantos@gmail.com>
2022-05-23 17:43:02 -0300

    criado componente ActiveLink e o arquivo api.json

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,21 @@
 import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
 import styles from './styles.module.scss';
 
-export function Header() {
-  const { asPath } = useRouter();
 
+export function Header() {
 
   return (
     <header className={styles.container}>
       <div className={styles.content}>
         <Image src="/logo.svg" width="225px" height="90px" alt="DevNews!" />
         <nav>
-          <Link href="/">
-            <a className={asPath === '/' ? styles.active : ''}>Home</a>  
-          </Link>
-          <Link href="/posts">
-            <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>  
-          </Link>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a >Home</a>  
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a >Posts</a>  
+          </ActiveLink>
         </nav>
       </div>
     </header>
