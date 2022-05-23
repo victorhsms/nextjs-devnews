a8b80ae25b4542945aa7c404ffe9a1018a3ca63b
Victor Hugo <victorhsmsantos@gmail.com>
2022-05-23 19:28:45 -0300

    adicionada página de posts estática

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,34 @@
-export default function Posts() {
-    return (
-      <div>
-        <h1>Listagem de posts</h1>
-        <ul>
-          {/* {posts.map((posts) => (
-            <li key={posts.id}>{posts.title}</li>
-          ))} */}
-        </ul>
-      </div>
-    )
+import { GetStaticProps } from "next";
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
+  return (
+    <div>
+      <h1>Listagem de posts</h1>
+      <ul>
+        {posts.map((posts) => (
+          <li key={posts.id}>{posts.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps<PostsProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts = await response.json();
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 5, // in seconds
   }
+}
