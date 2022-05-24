e7044e383c703a1b7e544f5503f5e3889cd6abbc
Victor Hugo <victorhsmsantos@gmail.com>
2022-05-24 13:52:21 -0300

    estilização da home

---
+++ src/pages/api/courses.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+const Courses = (request: NextApiRequest, response: NextApiResponse) => {
+  const courses = [
+    {id: 1, name: 'Next.js com Typescript'},
+    {id: 2, name: 'React.js com Typescript'},
+    {id: 3, name: 'Node.js com Typescript'},
+    {id: 4, name: 'SASS'},
+    {id: 5, name: 'Styled Component'},
+  ];
+
+  return response.json(courses);
+};
+
+export default Courses;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,28 @@
-import { GetServerSideProps } from "next";
-import { useEffect, useState } from "react";
+import SEO from "../components/SEO";
+import Image from 'next/image';
+import styles from '../styles/home.module.scss';
+import homeImage from '../../public/home.svg';
 
-interface Post {
-  id: string;
-  title: string;
-}
-
-interface HomeProps {
-  posts: Post[];
-}
-
-export default function Home({ posts }: HomeProps) {
-  // const [posts,setPosts] = useState<Post[]>([]);
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3333/posts').then((response) => {
-  //     response.json().then((data) => {
-  //       setPosts(data);
-  //     })
-  //   })
-  // }, [])
+export default function Home() {
   return (
     <>
-      <h1>Posts</h1>
-      <ul>
-        {posts.map((posts) => (
-          <li key={posts.id}>{posts.title}</li>
-        ))}
-      </ul>
+      <SEO title="Dev News!" excludeTitleSuffix/>
+      
+      <main className={styles.content}>
+        <section className={styles.section}>
+          <span>Olá Dev!</span>
+          <h1>
+            Boas vindas ao <br />
+            <span>Dev</span> News!
+          </h1>
+          <p>
+            Um blog com conteúdos extremamente <br />
+            <span>relevantes para o seu aprendizado</span>
+          </p>
+        </section>
+
+        <Image src={homeImage} alt="Home image" />
+      </main>
     </>
   )
-}
-
-export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
-  const response = await fetch('http://localhost:3333/posts');
-  const posts = await response.json();
-  return {
-    props: {
-      posts,
-    },
-  };
 }
